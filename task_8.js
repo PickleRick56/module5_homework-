8dc64ea6fa3ba50564dc9eacfbf1de907ed44cba
PickleRick56 <36300782+PickleRick56@users.noreply.github.com>
2023-01-07 22:05:42 +0500

    Задание 8.
    Создайте произвольный массив Map. Получите его ключи и выведите в консоль все значения в виде «Ключ — Х, значение — Y».
    
    Используйте шаблонные строки.

---
+++ task_8.js
@@ -0,0 +1,20 @@
+let fruits = new Map([
+
+    ["apple", "green"],
+  
+    ["strawberry", "red"],
+  
+    ["blueberry",    "blue"]
+  
+  ]);
+
+
+  for (let elem of fruits) { // то же самое, что и recipeMap.entries()
+
+    
+    console.log(`Ключ — ${elem[0]}, значение — ${elem[1]}`);
+ 
+ }
+
+
+
